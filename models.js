319a08e68e6893a015e916f674d041581559a446
ivencomur <ivancortes@hotmail.com>
2025-04-17 18:42:26 -0400

    no hash config

--- models.js
+++ models.js
@@ -12,7 +12,7 @@ const movieSchema = new mongoose.Schema({
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, 
+    password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     birthday: Date,
     favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
