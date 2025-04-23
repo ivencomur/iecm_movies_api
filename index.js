cf3e42db62db0202572d05b5088fbd8e7f4ed4f0
ivencomur <ivancortes@hotmail.com>
2025-04-23 17:41:52 -0400

    error correct

--- index.js
+++ index.js
@@ -90,8 +90,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-const cors = require("cors");
-app.use(cors());
+
 
 let auth = require("./auth")(app);
 app.use(passport.initialize());
