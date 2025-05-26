4163fcef5ad2da53510f39733ed5d2ddf30f9a49
ivencomur <ivancortes@hotmail.com>
2025-05-25 20:14:03 -0400

    per meeting (401 AND 500 ERRORS)

--- auth.js
+++ auth.js
@@ -22,20 +22,28 @@ module.exports = (router) => {
       if (error) {
         return next(error);
       }
+
+      //if user not defined or present return 401 network response 
       if (!user) {
         const message = (info && info.message) ? info.message : "Login failed (invalid credentials or user not found).";
         return res.status(401).json({ error: message });
       }
+
+      //otherwise the user object came through login in
       req.login(user, { session: false }, (error) => {
         if (error) {
           return next(error);
         }
+
+        //create token
         try {
           const token = generateJWTToken(user.toJSON());
+          //token not defined return 500 error
           if (!token) {
             return res.status(500).json({ error: "Internal server error: Could not generate token." });
           }
           const userResponse = { ...user.toJSON() };
+          //remove password from response so not sent back over network
           delete userResponse.password;
           return res.status(200).json({ user: userResponse, token: token });
         } catch (jwtError) {
