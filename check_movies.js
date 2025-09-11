74fa9e705e5077819d6d2b6597b6e5ed92c725a1
ivencomur <ivancortes@hotmail.com>
2025-09-11 02:14:27 -0400

    STARTING TO WORK

---
+++ check_movies.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+async function checkMovies() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    const count = await Models.Movie.countDocuments();
+    console.log(`Total movies in database: ${count}`);
+    
+    if (count > 0) {
+      const movies = await Models.Movie.find().limit(3);
+      console.log('Sample movies:');
+      movies.forEach(movie => console.log(`- ${movie.Title || movie.title}`));
+    } else {
+      console.log('No movies found - database needs to be seeded');
+    }
+    process.exit(0);
+  } catch (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+}
+checkMovies();
--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const corsOptions = {
         "http://localhost:3000", // Common React dev server port
         "http://localhost:5173",
         "http://localhost:52020",
+        "http://localhost:4200",
         "https://themoviemobs.netlify.app"
 
       ].includes(origin)
