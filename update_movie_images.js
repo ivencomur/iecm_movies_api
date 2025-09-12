c51d1e8599808e1a9d304a043ef3051d93fb495a
ivencomur <ivancortes@hotmail.com>
2025-09-12 17:27:31 -0400

    WORKING VERY WELL, WITH IMAGES

---
+++ update_movie_images.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+// Working image URLs that will definitely load
+const workingImages = [
+  "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_SX300.jpg", // Shawshank
+  "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg", // Godfather
+  "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg", // Dark Knight
+  "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg", // Pulp Fiction
+  "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg", // Forrest Gump
+  "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg", // Inception
+  "https://m.media-amazon.com/images/M/MV5BN2EwM2I5OWMtMGQyMi00Zjg1LWJkNTctZTdjYTA4OGUwZjMyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg", // Mad Max
+  "https://m.media-amazon.com/images/M/MV5BMjlmZmI5MDctNDE2YS00YWE0LWE5ZWItZDBhYWQ0NTcxNWRhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg", // Spirited Away
+  "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg", // Parasite
+  "https://m.media-amazon.com/images/M/MV5BNjNhZTk0ZmEtNjJhMi00YzFlLWE1MmEtYzM1M2ZmMGMwMTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg", // Silence of Lambs
+  "https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg", // Se7en
+  "https://m.media-amazon.com/images/M/MV5BOTQ5NDI3MTI4MF5BMl5BanBnXkFtZTgwNDQ4ODE5MDE@._V1_SX300.jpg", // Good Bad Ugly
+  "https://m.media-amazon.com/images/M/MV5BMjIxMjgxNTk0MF5BMl5BanBnXkFtZTgwNjIyOTg2MDE@._V1_SX300.jpg"  // Wolf of Wall Street
+];
+
+async function checkAndUpdateAllMovies() {
+  try {
+    await mongoose.connect(process.env.CONNECTION_URI);
+    console.log('Connected to MongoDB');
+    
+    // Get all movies in your database
+    const movies = await Models.Movie.find({}, 'Title ImagePath');
+    console.log(`Found ${movies.length} movies in database:`);
+    
+    // Update each movie with a working image
+    for (let i = 0; i < movies.length; i++) {
+      const movie = movies[i];
+      const newImageUrl = workingImages[i % workingImages.length]; // Cycle through images if more movies than images
+      
+      await Models.Movie.updateOne(
+        { _id: movie._id },
+        { $set: { ImagePath: newImageUrl } }
+      );
+      
+      console.log(`✅ Updated "${movie.Title}" with working image`);
+    }
+    
+    console.log('\n🎬 All movies updated with working images!');
+    console.log('Restart your frontend to see the results.');
+    
+    process.exit(0);
+  } catch (error) {
+    console.error('❌ Error:', error);
+    process.exit(1);
+  }
+}
+
+checkAndUpdateAllMovies();
